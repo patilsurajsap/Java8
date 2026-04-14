f6df19f0efd632f73ce48906390d51ba211ea88c
suraj <patil.suraj.sap@gmail.com>
2026-04-14 20:12:22 +0530

    java stream

---
+++ src/main/java/org/example/StreamSorted.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import com.java8.factory.StudentFactory;
+
+public class StreamSorted {
+
+
+    StudentFactory studentFactory = new StudentFactory();
+
+    public static void main(String[] args) {
+            StudentFactory studentFactory = new StudentFactory();
+
+            studentFactory.getStudentList().stream().sorted((s1, s2) -> s2.getName().compareTo(s1.getName()))
+                    .forEach(student -> System.out.println(student.getName()));
+    }
+}
