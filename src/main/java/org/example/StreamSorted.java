25423808106ca78086ffb054063ef61fb27ac918
suraj <patil.suraj.sap@gmail.com>
2026-04-14 20:14:17 +0530

    java stream

--- src/main/java/org/example/StreamSorted.java
+++ src/main/java/org/example/StreamSorted.java
@@ -12,5 +12,10 @@ public class StreamSorted {
 
             studentFactory.getStudentList().stream().sorted((s1, s2) -> s2.getName().compareTo(s1.getName()))
                     .forEach(student -> System.out.println(student.getName()));
+
+       System.out.println("==================================");
+
+        studentFactory.getStudentList().stream().sorted((s1, s2) -> s1.getName().length() - s2.getName().length())
+                .forEach(student -> System.out.println(student.getName()));
     }
 }
