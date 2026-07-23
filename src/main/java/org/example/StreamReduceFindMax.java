2c5208270e24bfc2eaba1876024e162375019ce0
suraj <patil.suraj.sap@gmail.com>
2026-07-23 14:49:36 +0530

    java stream

--- src/main/java/org/example/StreamReduceFindMax.java
+++ src/main/java/org/example/StreamReduceFindMax.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.OptionalInt;
+
 public class StreamReduceFindMax {
 
     public static void main(String[] args) {
@@ -8,6 +11,10 @@ public class StreamReduceFindMax {
 
          int max = java.util.Arrays.stream(arr).reduce(Integer.MIN_VALUE, (a, b) -> a > b ? a : b);
 
-        System.out.println(max);
+        OptionalInt min = Arrays.stream(arr).reduce( (a, b) -> a < b ? a : b);
+
+        OptionalInt min1 = Arrays.stream(arr).min();
+
+        System.out.println(max + ":" + min.getAsInt() + ":" + min1.getAsInt());
     }
 }
